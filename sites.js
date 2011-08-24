65a16604d884afac1e71b9ffe8deb1999e35385b
airsun <sunjj.public@gmail.com>
2011-08-24 17:20:10 +0800

    proto style

---
+++ sites.js
@@ -0,0 +1,26 @@
+sites = [
+    {
+        title:"测试site",
+        url:"http://jquery.com",
+        icon:"",
+        rss:"http://forum.jquery.com/getting-started/feed"
+    },
+    {
+        title:"wiki",
+        url:"http://192.168.4.26:8090",
+        icon:"",
+        rss:"rss a"
+    },
+    {
+        title:"jira",
+        url:"http://192.168.4.249:8090",
+        icon:"",
+        rss:"rss b"
+    },
+    {
+        title:"mail",
+        url:"mail.xxx.com",
+        icon:"",
+        rss:"rss b"
+    }
+];
